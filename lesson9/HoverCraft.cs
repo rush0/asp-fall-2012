96e6c33ab31904ed1da918fe1d20d624cf556b67
Omar Stewey <OmarStewey@gmail.com>
2012-08-01 20:55:40 -0400

    Implicit vs Explicit Interface Implementation
    A Hovercraft is an Aircraft and a Vehicle

---
+++ lesson9/HoverCraft.cs
@@ -0,0 +1,36 @@
+﻿namespace ExplicitVsImplicit
+{
+    // Vehicle Interface
+    interface IVehicle
+    {
+        string Accelerate();
+    }
+
+    // Aircraft Interface
+    interface IAirCraft
+    {
+        string Accelerate();
+    }
+
+    // Hovercraft 
+    class HoverCraft : IAirCraft, IVehicle
+    {
+        // How should I Accelerate ?
+
+        // Implicit Implementation will confuse me
+        string Accelerate() { return "Confused, Crash"; }
+
+        // Explicit Implementation
+        string IAirCraft.Accelerate()
+        {
+            return "Preparing for Lift Off!";
+        }
+
+        string IVehicle.Accelerate()
+        {
+            return "Step on the Gas!";
+        }
+    }
+
+    
+}
